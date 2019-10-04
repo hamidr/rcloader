1ed3905bd92b7131842d1820670b5b84209d6ceb
Hamidr <hamid@fastforward.ai>
2019-10-04 10:41:35 +0200

    compile it

--- src/printer.rs
+++ src/printer.rs
@@ -1,9 +1,5 @@
 use crate::node::Node;
-use serde_json::Value as JsonValue;
 
-fn form_json(nodes: &Vec<Node> ) -> JsonValue {
-
-}
 pub fn to_json(_nodes: &Vec<Node>) -> String {
     println!("{:?}", _nodes); "".to_string()
 }
